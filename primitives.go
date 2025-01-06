7a4434c30ae23a2da4ce0c7b07bfb4a3416258e3
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-06 00:02:27 -0500

    Add select statement

--- primitives.go
+++ primitives.go
@@ -19,14 +19,26 @@ import "fmt"
 
 func main() { // main function and func is go routine join together
 	myChannel := make(chan string) // make a channel that passes string type data
-
+	anotherChannel := make(chan string)
 	// go routine of an anonymous function, forked away and executing on its own.
 	go func() {
 		myChannel <- "data" // sending data to myChannel
 	}()
 
-	// below is blocking, main function is waiting for the channel to close or a message to be received from the channel
-	msg := <-myChannel // main function is reading data from the channel, blocking line of code is the join point
+	go func() {
+		anotherChannel <- "cow"
+	}()
 
-	fmt.Println(msg)
+	select { // select statement, block until one of the cases can run, will choose one at random.
+	case msgFromMyChannel := <-myChannel:
+		fmt.Println(msgFromMyChannel)
+	case msgFromAnotherChannel := <-anotherChannel:
+		fmt.Println(msgFromAnotherChannel)
+	}
+	// block here means anything below these codes will only execute after the block is finish
+
+	// // below is blocking, main function is waiting for the channel to close or a message to be received from the channel
+	// msg := <-myChannel // main function is reading data from the channel, blocking line of code is the join point
+
+	// fmt.Println(msg)
 }
